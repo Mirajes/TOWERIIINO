5343b76fa41083eb0b6ad3ea2d8e8383de2df30f
Mirajes <myrrrclone@gmail.com>
2025-10-07 10:30:06 +0300

    Initial commit

---
+++ Assets/Scripts/Balance.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Balance", menuName = "Scriptable Objects/Balance")]
+public class Balance : ScriptableObject
+{
+    [SerializeField] private int a = 0;
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Timer _timer = new ();
+
+    private void Awake()
+    {
+        
+    }
+}
